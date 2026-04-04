e40c48b45d4acd797f9c5362513213731ab6e563
Pratyush8678 <kumarpratyush574@gmail.com>
2026-04-04 17:18:32 +0530

    Login and Logout

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -2,6 +2,10 @@ import userModel from "../user.model";
 import { ErrorType } from "../error/error";
 import { sendError, userCreateSuccess } from "../utils/response";
 import argon from "argon";
+import jwt from "jsonwebtoken";
+
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
 export async function register(req, res) {
   const { username, email, password } = req.body;
@@ -15,7 +19,7 @@ export async function register(req, res) {
       $or: [{ email }, { username }],
     });
 
-    if (isAlreadyRegister)  {
+    if (isAlreadyRegister) {
       return sendError(res, ErrorType.EMAIL_USERNAME_EXISTS);
     }
 
@@ -32,17 +36,89 @@ export async function register(req, res) {
 }
 
 export async function login(req, res) {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return sendError(res, ErrorType.ALL_FIELD_IS_IMPORTANT);
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return sendError(res, ErrorType.ALL_FIELD_IS_IMPORTANT);
+    }
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return sendError(res, ErrorType.INVALID_REQUEST);
+    }
+    const isPasswordValid = await argon.verify(user.password, password);
+    if (!isPasswordValid) {
+      return sendError(res, ErrorType.INVALID_REQUEST);
+    }
+
+    const accessToken = jwt.sign(
+      { id: user._id, email: user.email },
+      ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" },
+    );
+
+    const refreshToken = jwt.sign({ id: user._id }, REFRESH_TOKEN_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json({
+      message: "logged in successfully",
+      accessToken,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    return sendError(res, ErrorType.INTERNAL_SERVER_ERROR);
   }
-  const user = await userModel.findOne({ email });
-  if (!user) {
-    return sendError(res, ErrorType.INVALID_REQUEST);
+}
+
+export async function refresh(req, res) {
+  try {
+    const token = req.cookie.refreshToken;
+    if (!token) {
+      return sendError(res, ErrorType.INVALID_REQUEST);
+    }
+    const decoded = jwt.verify(token, REFRESH_TOKEN_SECRET);
+
+    const user = await userModel.findOne({
+      _id: decoded._id,
+      refreshToken: token,
+    });
+    if (!user) {
+      return sendError(res, ErrorType.INVALID_REQUEST);
+    }
+
+    const newAccessToken = jwt.sign(
+      { id: user._id, email: user.email },
+      ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" },
+    );
+    return res.status(200).json({
+      accessToken: newAccessToken,
+    });
+  } catch (error) {
+    return sendError(res, ErrorType.INTERNAL_SERVER_ERROR);
   }
-  const isPasswordValid = await argon.verify(user.password, password);
-  if(!isPasswordValid){
-    return sendError(res,ErrorType.INVALID_REQUEST)
+}
+export async function logout(req, res) {
+  try {
+    const token = req.cookie.refreshToken;
+    if (token) {
+      const decoded = jwt.verify(token, REFRESH_TOKEN_SECRET);
+      await userModel.findByIdAndUpdate(decoded.id, { refreshToken: null });
+    }
+    res.clearCookie("refreshToken");
+    return res.status(200).json({ message: "Logout successfully" });
+  } catch (error) {
+    return sendError(res, ErrorType.INTERNAL_SERVER_ERROR);
   }
-  
 }
